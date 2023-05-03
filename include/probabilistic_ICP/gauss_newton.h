9d54cb60fc1ca955454a91f922bb693b7f584755
hamdaan19 <md.hamdaan19@gmail.com>
2023-05-03 23:19:21 +0530

    added objective func + changes

--- include/probabilistic_ICP/gauss_newton.h
+++ include/probabilistic_ICP/gauss_newton.h
@@ -7,18 +7,28 @@ namespace optim {
 
     class GaussNewton {
         public: 
-            GaussNewtom(
-                std::vector<Eigen::Vector<double,2> z, // Measurement Vector
-                std::vector<Eigen::Vector<double,2> m, // Previous scan points
+            GaussNewton(
+                std::vector<Eigen::VectorXd> z, // Measurement Vector
+                std::vector<Eigen::VectorXd> m, // Previous scan points
                 // Ptr to function
                 // Arg list: 2D point in previous scan, state 
-                Eigen::Vector<double,2>(*obs_model)(Eigen::Vector2d&, Eigen::Vector3d&),
-                Eigen::Matrix<double,2,2> Q, // observation model uncertainty
+                Eigen::VectorXd(*obs_model)(Eigen::Vector2d&, Eigen::Vector3d&),
+                // Arg list: 2D point in previous scan, current state, previous state
+                Eigen::Matrix<double,-1,-1>(*get_obs_jacobi)(Eigen::Vector2d&, Eigen::Vector3d&, Eigen::Vector3d&),
+                Eigen::MatrixXd Q, // observation model uncertainty
+                Eigen::VectorXd x_prev
             );
+
+            Eigen::VectorXd(*observation_model)(Eigen::Vector2d&, Eigen::Vector3d&); 
+            Eigen::Matrix<double,-1,-1>(*get_obs_jacobian)(Eigen::Vector2d&, Eigen::Vector3d&, Eigen::Vector3d&); 
+            std::vector<Eigen::VectorXd> z_arr; 
+            std::vector<Eigen::VectorXd> m_arr;
+            Eigen::MatrixXd Q; 
+            Eigen::VectorXd x_t0; 
 
             double objective_func(
                 Eigen::Vector3d x,               // state
-                Eigen::Vector3d mew,             // Predicted state after applying 
+                Eigen::Vector3d mew,             // Predicted (prior) state via motion model
                 Eigen::Matrix<double,3,3> S_t0,  // Corrected previous state covariance 
                 Eigen::Matrix<double,3,3> S_t1_, // Predicted state covariance after motion model is applied
             ); 
--- src/gauss_newton.cpp
+++ src/gauss_newton.cpp
@@ -1,32 +1,52 @@
-#include <probabilistic_ICP/guass_newton.h>
+#include <probabilistic_ICP/gauss_newton.h>
 #include <cmath>
 
-Eigen::Matrix<double,2,3> optim::GaussNewton::get_obs_jacobian(Eigen::Vector2d& m, Eigen::Vector3d& x, Eigen::Vector3d& mew){
-    dx = x - mew; 
+#include <numeric> 
 
-    Eigen::Matrix2d dR << // relative rotation
-    cos(dx(2)), -sin(dx(2)),
-    sin(dx(2)), cos(dx(2));
+optim::GaussNewton::GaussNewton(
+    std::vector<Eigen::VectorXd> z, // Measurement Vector
+    std::vector<Eigen::VectorXd> m, // Previous scan points
+    // Ptr to function
+    // Arg list: 2D point in previous scan, state 
+    Eigen::VectorXd(*obs_model)(Eigen::Vector2d&, Eigen::Vector3d&),
+    // Arg list: 2D point in previous scan, current state, previous state
+    Eigen::Matrix<double,-1,-1>(*get_obs_jacobi)(Eigen::Vector2d&, Eigen::Vector3d&, Eigen::Vector3d&),
+    Eigen::MatrixXd Q, // observation model uncertainty
+    Eigen::VectorXd x_prev
+) : observation_model(obs_model), get_obs_jacobian(get_obs_jacobi), z_arr(z), m_arr(m), Q(Q), x_t0(x_prev) {
 
-    Eigen::Vector2d dt << dx(0), dx(1); // relative translation
+}
 
-    Eigen::Matrix3d dT_rev = Eigen::Matrix3d::Identity(); // relative reverse transformation
-    dT_rev.block(0,0,2,2) = dR.tranpose(); 
-    dT_rev.block(0,2,2,1) = -dt; 
+optim::GaussNewton::objective_func(
+    Eigen::Vector3d x,               // state
+    Eigen::Vector3d mew,             // Predicted (prior) state via motion model
+    Eigen::Matrix<double,3,3> S_t0,  // Corrected previous state covariance 
+    Eigen::Matrix<double,3,3> S_t1_, // Predicted state covariance after motion model is applied
+) {
 
-    Eigen::Vector3d m_homo << m(0), m(1), 1.0; // previous scan corresponding point in homogeneous coordinates
+    // H = get_obs_jacobian()
+    // S_z = S_t0
 
-    auto m_cap_homo = dT_rev*m_homo; 
+    std::vector<unsigned> meas_idx(z.size()); 
+    std::iota(meas_idx.begin(), meas_idx.end(), 0); 
 
-    double dow_h1_by_dow_x = -1 * m_cap_homo(0) * pow( pow(m_cap_homo(0),2) + pow(m_cap_homo(1),2), -0.5); 
-    double dow_h1_by_dow_y = -1 * m_cap_homo(1) * pow( pow(m_cap_homo(0),2) + pow(m_cap_homo(1),2), -0.5); 
-    double dow_h1_by_dow_theta = pow( pow(m_cap_homo(0),2) + pow(m_cap_homo(1),2), -0.5) * (
-        m_cap_homo(0) * (m_homo(0)*sin(dx(2)) - m_homo(1)*cos(dx(2))) +
-        m_cap_homo(1) * (m_homo(0)*cos(dx(2)) + m_homo(1)*sin(dx(2))) ) ;
+    auto measurement_part = [&z_arr, &m_arr, &S_t0, Q, x, x_t0](unsigned int i) {
+        Eigen::VectorXd z_i = z_arr.at(i); 
+        Eigen::VectorXd m_i = m_arr.at(i); 
 
-     
+        H = get_obs_jacobian(m_i, x, x_t0); 
+        S_z = H*S_t0*H.transpose() + Q;
+        auto z_est = observation_model(m_i, x, x_t0);
 
+        double res = (z_i - z_est).transpose() * S_z.inverse() * (z_i - z_est); 
 
+        return res; 
+    }
 
+    double cost1 = std::accumulate(meas_idx.begin(), meas_idx.end(), 0.0, measurement_part); 
+
+    double cost2 = (mew - x).transpose() * S_t1_.inverse() * (mew - x); 
+
+    return cost1 + cost2; 
 
 }
